9230edbf5f3678c3b9b7dbd0ca922f10a610fffb
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2023-09-03 16:22:59 +0100

    updated to 1.5.1.3

--- scripts/update.sh
+++ scripts/update.sh
@@ -91,11 +91,13 @@ function update_chugins() {
 
 
 function fix_chuck_tilde() {
+	echo
 	# replace "msg->reply" "msg->reply_cb" ${CHUCK_TILDE}
 	# patch chuck~/chuck~.cpp < scripts/patch/chuck_xxxx.patch
 }
 
 function apply_all_patches() {
+	echo
 	# empty for now
 }
 
